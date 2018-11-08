759653517cd3649de48a567e82a2be7829979e1e
mezitomi <mezitomi@gmail.com>
2018-11-08 22:39:46 +0100

    imageVM and VoteVM covered.

---
+++ gRate.Test/ImageViewModelTests.cs
@@ -0,0 +1,49 @@
+﻿using gRATE.ViewModels;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace gRate.Test
+{
+    [TestFixture]
+    internal class ImageViewModelTests
+    {
+        [Test]
+        public void SanityCheck()
+        {
+            Assert.NotNull(new ImageViewModel());
+        }
+
+        [Test]
+        public void TestEmptyCtor_fieldsAreGiven_fieldsAreSaved()
+        {
+            ImageViewModel model = new ImageViewModel()
+            {
+                Category = gRATE.Models.Category.Animal,
+                Description = "testDesciption",
+                DesiredVoteCount = gRATE.Models.DesiredVoteCount.M,
+                Path = "testPath",
+                Title = "TestingTesting1234"
+            };
+
+            List<object> expectedFields = new List<object>()
+            {
+               gRATE.Models.Category.Animal,
+                "testDesciption",
+                gRATE.Models.DesiredVoteCount.M,
+                "testPath",
+                "TestingTesting1234"
+            };
+
+            List<object> actualFields = new List<object>()
+            {
+                model.Category,
+                model.Description,
+                model.DesiredVoteCount,
+                model.Path,
+                model.Title
+            };
+
+            Assert.AreEqual(expectedFields, actualFields);
+        }
+    }
+}
---
+++ gRate.Test/VoteViewModelTests.cs
@@ -0,0 +1,44 @@
+﻿using gRATE.ViewModels;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gRate.Test
+{
+    [TestFixture]
+    class VoteViewModelTests
+    {
+        [Test]
+        public void SanityCheck()
+        {
+            Assert.NotNull(new VoteViewModel());
+        }
+
+        [Test]
+        public void TestEmptyCtor_fieldsAreGiven_fieldsAreSaved()
+        {
+            VoteViewModel model = new VoteViewModel()
+            {
+                ImageId = 1,
+                UserId = 2,
+                VoteValue = 2
+            };
+
+            List<object> expectedFields = new List<object>()
+            {
+                1,2,2
+            };
+
+            List<object> actualFields = new List<object>()
+            {
+                model.ImageId,
+                model.UserId,
+                model.VoteValue
+            };
+
+            Assert.AreEqual(expectedFields, actualFields);
+
+        }
+    }
+}
